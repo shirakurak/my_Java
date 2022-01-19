0ce5c5c04fa3858cc9f63762abbac70e41bb1030
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-20 08:28:02 +0900

    ByteUtils.getByteImagesを作成

---
+++ src/demo/src/main/java/com/example/demo/utils/ByteUtils.java
@@ -0,0 +1,26 @@
+package com.example.demo.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ByteUtils {
+	
+	/**
+	 * convert image to byte[]
+	 * @param name name of image want to convert to byte[]
+	 * @return image want to convert to byte[]
+	 * @throws IOException exception when reading the file
+	 */
+	public byte[] getByteImages(String name) throws IOException {
+		
+		String path = 
+			Paths.get("").toAbsolutePath().toString()	// カレントパス
+			+ "\\src\\main\\resources\\images\\"
+			+ name;
+		File file = new File(path);
+		
+		return  Files.readAllBytes(file.toPath());
+	}
+}
