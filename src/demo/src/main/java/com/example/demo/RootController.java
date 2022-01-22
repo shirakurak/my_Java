691615800451a26cb99311817d9b41befcba7905
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-23 08:57:36 +0900

    logger追加

--- src/demo/src/main/java/com/example/demo/RootController.java
+++ src/demo/src/main/java/com/example/demo/RootController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class RootController {
 	
+	Logger logger = LoggerFactory.getLogger(RootController.class);
+	
 	@RequestMapping("/")
 	public String home() {
+		
+		logger.info("here is RootController");
+		
 		return "Hello, World!";
 	}
 }
