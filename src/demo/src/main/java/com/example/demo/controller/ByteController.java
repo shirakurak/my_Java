be44e5e092dd666fb6be5d8313c2c82075478538
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-22 11:41:37 +0900

    base64.encode追加

--- src/demo/src/main/java/com/example/demo/controller/ByteController.java
+++ src/demo/src/main/java/com/example/demo/controller/ByteController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,5 +21,22 @@ public class ByteController {
 	public ResponseEntity<byte[]> home() throws IOException {
 		byte[] byteImage = util.getByteImages("sample.jpg");
 		return new ResponseEntity<byte[]>(byteImage, HttpStatus.OK);
+	}
+	
+	/**
+	 * 以下を参考：
+	 * https://gist.github.com/komiya-atsushi/d878e6e4bf9ba6dae8fa
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping("/byte2")
+	public ResponseEntity<String> home2() throws IOException {
+		byte[] byteImage = util.getByteImages("sample.jpg");
+		
+		String encoded = Base64.getEncoder()
+				.encodeToString(byteImage);
+		
+		return new ResponseEntity<String>(encoded, HttpStatus.OK);
 	}
 }
