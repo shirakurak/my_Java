23628af8151dde96c8a140060f9e30e142595c4b
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-25 08:00:15 +0900

    空行削除

--- src/demo/src/main/java/com/example/demo/RootController.java
+++ src/demo/src/main/java/com/example/demo/RootController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class RootController {
 	
