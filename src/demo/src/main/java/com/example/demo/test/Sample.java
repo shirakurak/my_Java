86b7bc7a6ed0b7235701f1db611b4ea7662522b7
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-25 08:00:37 +0900

    すぐ使わなそうなクラス、コメントアウト

--- src/demo/src/main/java/com/example/demo/test/Sample.java
+++ src/demo/src/main/java/com/example/demo/test/Sample.java
@@ -2,8 +2,8 @@ package com.example.demo.test;
 
 public class Sample {
 
-	public static void main(String[] args) {
-		// 動作検証用クラス
-	}
+//	public static void main(String[] args) {
+//		// 動作検証用クラス
+//	}
 
 }
